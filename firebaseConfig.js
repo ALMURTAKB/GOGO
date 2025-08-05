8df4ed7ae85494c20e26167b3b1217d1c8622710
ALMURTAKB <haydernnn@gmail.com>
2025-08-05 11:45:16 +0300

    Update firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,11 @@
+// firebaseConfig.js
+const firebaseConfig = {
+  apiKey: "AIzaSyCxZKjnVEVjVBbgwAOUUbEg9D4KbEJqUG4",
+  authDomain: "gogo-580c9.firebaseapp.com",
+  projectId: "gogo-580c9",
+  storageBucket: "gogo-580c9.appspot.com",
+  messagingSenderId: "1017270070832",
+  appId: "1:1017270070832:web:802137b9719a1c525747f5",
+  measurementId: "G-YRNMSSYD1W"
+};
+export default firebaseConfig;
