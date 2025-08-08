8ac518e91943b33306d7656c918dd63a718710e2
ALMURTAKB <haydernnn@gmail.com>
2025-08-08 09:34:23 +0300

    Delete firebaseConfig.js

--- firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-// firebaseConfig.js
-const firebaseConfig = {
-  apiKey: "AIzaSyCxZKjnVEVjVBbgwAOUUbEg9D4KbEJqUG4",
-  authDomain: "gogo-580c9.firebaseapp.com",
-  projectId: "gogo-580c9",
-  storageBucket: "gogo-580c9.appspot.com",
-  messagingSenderId: "1017270070832",
-  appId: "1:1017270070832:web:802137b9719a1c525747f5",
-};
